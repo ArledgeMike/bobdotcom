8c858314d81120acb63ab995e13a8c6bc3e666fa
Michael Arledge <arledgemike@gmail.com>
2014-10-08 12:05:18 -0500

    QA testing of slack bot

--- slack_bot.rb
+++ slack_bot.rb
@@ -6,17 +6,12 @@ class Slack_Bot
 def initialize(username, image_title)
   @user = username
   @title = image_title
-  params =  { :token => "xoxp-2538560016-2543438978-2768701902-c864db", :username => "TESTBOT-NINER", :channel => "C02NRLGSF", :text => "#{@user} added the file #{@title} to www.bobdotbiz.com"  }
+  params =  { :token => "xoxp-2538560016-2543438978-2768701902-c864db", :username => "011001100111010101100011011010110110001001100101011000010110111001110011", :channel => "C02NTNN8E", :text => "#{@user} has added an ideation titled: #{@title} to www.bobdotbiz.com"  }
 
   url = URI("https://slack.com/api/chat.postMessage");
-
   url.query = URI.encode_www_form(params);
-
   res = Net::HTTP.get(url)
 
-  puts res
-
   end
-
 
 end
